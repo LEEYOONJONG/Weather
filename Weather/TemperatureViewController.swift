25eed77b9ad1bfdb165e0e3878420a07db10957b
LEEYOONJONG <yoonjong1820@khu.ac.kr>
2021-08-03 21:47:36 +0900

    Set layout for Nested Scroll VIew

---
+++ Weather/TemperatureViewController.swift
@@ -0,0 +1,17 @@
+//
+//  TemperatureViewController.swift
+//  Weather
+//
+//  Created by YOONJONG on 2021/08/03.
+//
+
+import UIKit
+
+class TemperatureViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
