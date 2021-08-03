5a5c2806822d409fc8493300808f1c5d9a06bbf5
LEEYOONJONG <yoonjong1820@khu.ac.kr>
2021-08-03 23:30:16 +0900

    Add horizontal scroll view

--- Weather/TemperatureViewController.swift
+++ Weather/TemperatureViewController.swift
@@ -15,3 +15,31 @@ class TemperatureViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 }
+
+extension TemperatureViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 20
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "TemperatureCell", for: indexPath) as? TemperatureCell else { return UICollectionViewCell() }
+        // let info = viewmodel.info(At:indexpath.item)
+        // cell.update(info:info)
+        return cell;
+    }
+    
+    
+}
+
+extension TemperatureViewController: UICollectionViewDelegate {
+    // 필요없을듯. 클릭했을때의 동작
+}
+
+
+class TemperatureCell: UICollectionViewCell {
+    @IBOutlet weak var weatherImage: UIImageView!
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    
+    
+}
