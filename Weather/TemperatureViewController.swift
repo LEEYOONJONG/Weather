74d8b6fbd3945f25056251bf5d878fddb0af1c28
LEEYOONJONG <yoonjong1820@khu.ac.kr>
2021-08-06 17:57:57 +0900

    Add update (can't update collectionview)

--- Weather/TemperatureViewController.swift
+++ Weather/TemperatureViewController.swift
@@ -6,27 +6,30 @@
 //
 
 import UIKit
+import Foundation
 
 class TemperatureViewController: UIViewController {
 
+    @IBOutlet weak var temperatureCollectionView: UICollectionView!
+    
     let viewModel = HourlyViewModel()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        viewModel.fetchData()
+       
     }
     
 }
 
 extension TemperatureViewController: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 20
+        return viewModel.HourlyList.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "TemperatureCell", for: indexPath) as? TemperatureCell else { return UICollectionViewCell() }
-        // let info = viewmodel.info(At:indexpath.item)
-        // cell.update(info:info)
+        let info = viewModel.HourlyList[indexPath.item]
+        cell.update(info:info)
         return cell;
     }
     
@@ -43,17 +46,27 @@ class TemperatureCell: UICollectionViewCell {
     @IBOutlet weak var timeLabel: UILabel!
     @IBOutlet weak var temperatureLabel: UILabel!
     
+    func update(info:Hourly){
+        timeLabel.text = "\(info.dt)"
+        temperatureLabel.text = "\(info.temp)"
+    }
 }
 
 class HourlyViewModel {
     var HourlyList:[Hourly]=[]
-    func fetchData(){
+    func fetchData() -> Void{
         WeatherService().getWeather { result in
             print("위도 : \(result.lat), 경도 : \(result.lon)")
 //            print("--> ", result.hourly)
             let hourlyCnt:Int = result.hourly.count
             for i in 0..<hourlyCnt{
                 print("\(i)번째 data : \(result.hourly[i])")
+//                let date = Date(timeIntervalSince1970: TimeInterval(result.hourly[i].dt))
+//                let dateFormatter = DateFormatter()
+//                dateFormatter.timeZone = TimeZone(abbreviation: "KST")
+//                dateFormatter.dateFormat = "yyyy-MM-dd HH:mm"
+//                let stringDate = dateFormatter.string(from: date) // UNIX timestamp 형식을 KST로 변환
+//                print("date: ", stringDate)
                 self.HourlyList.append(Hourly(dt: result.hourly[i].dt, temp: result.hourly[i].temp, humidity: result.hourly[i].humidity))
             }
         }
