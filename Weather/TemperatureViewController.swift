68497b7838f2089923ed39887b6838ba63728ed6
LEEYOONJONG <yoonjong1820@khu.ac.kr>
2021-08-07 14:46:23 +0900

    Hide Secret Key

--- Weather/TemperatureViewController.swift
+++ Weather/TemperatureViewController.swift
@@ -13,19 +13,28 @@ class TemperatureViewController: UIViewController {
     @IBOutlet weak var temperatureCollectionView: UICollectionView!
     
     let viewModel = HourlyViewModel()
-    
+    private var apiKey:String{
+        get {
+            guard let filePath = Bundle.main.path(forResource: "Property List", ofType: "plist")
+            else { return "" }
+            let plist = NSDictionary(contentsOfFile: filePath)
+            print("plist : ",plist)
+            guard let value = plist?.object(forKey: "OPENWEATHERMAP_KEY") as? String else { return "" }
+            return value
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         temperatureCollectionView.delegate = self
-        
         fetchWeather()
     }
+    
     func fetchWeather(){
         let session = URLSession(configuration: URLSessionConfiguration.default)
         print("getWeather called")
         // API 호출을 위한 URL
         
-        let url = URL(string: "https://api.openweathermap.org/data/2.5/onecall?lat=37.491&lon=126.980&exclude=minutely,daily&appid=eb70946e865f0ed18c6eb6e2a4a39651&units=metric")
+        let url = URL(string: "https://api.openweathermap.org/data/2.5/onecall?lat=37.491&lon=126.980&exclude=minutely,daily&appid=\(apiKey)&units=metric")
         let dataTask = session.dataTask(with: url!){ (data, response, error) in
             guard error == nil else { return }
             guard let resultData = data else { return }
